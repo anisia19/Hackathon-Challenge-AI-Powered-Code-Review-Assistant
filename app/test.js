4921b9cb6c8401cc613935985418300d644a803f
anisia19 <anisia.fort19@gmail.com>
2025-11-01 18:30:29 +0200

    add test code

---
+++ app/test.js
@@ -0,0 +1,24 @@
+/**
+ * Calculează media unui array de numere.
+ * Are un bug: folosește operatorul gresit pentru suma.
+ */
+function calculateAverage(numbers) {
+    if (!numbers || numbers.length === 0) {
+        return 0;
+    }
+
+    let sum = 0;
+    // BUG: Folosește operatorul de concatenare '+' în loc de atribuire '+=' la primul element.
+    for (let i = 0; i < numbers.length; i++) {
+        if (i === 0) {
+            sum = sum + numbers[i]; // Corect ar fi: sum += numbers[i]
+        } else {
+            sum += numbers[i];
+        }
+    }
+
+    // De fapt, codul de mai sus ar trebui să fie:
+    // for (let i = 0; i < numbers.length; i++) { sum += numbers[i]; }
+
+    return sum / numbers.length;
+}
